7c3c02173ce3029b2ef6da7d9e90919c92422556
Basil <basil.0522002@gmail.com>
2024-10-12 21:44:23 +0300

    working on thank you page

--- lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
+++ lib/Features/checkout/presentation/views/widgets/thank_you_view_body.dart
@@ -5,6 +5,29 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(children: [
+      Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Container(
+          decoration: ShapeDecoration(
+              color: const Color(0xffD9D9D9),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              )),
+        ),
+      ),
+      Positioned(
+          left: 15,
+          bottom: MediaQuery.of(context).size.height * 0.2,
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+          )),
+      Positioned(
+          right: 15,
+          bottom: MediaQuery.of(context).size.height * 0.2,
+          child: CircleAvatar(
+            backgroundColor: Colors.white,
+          )),
+    ]);
   }
 }
